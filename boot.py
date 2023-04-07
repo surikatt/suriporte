6bbc50f27cf6d69bbd56c369d17602e074fe1770
Melvil Ferreira <melvil.ferreira@gmail.com>
2023-04-07 14:23:30 +0200

    Update 07/04

--- boot.py
+++ boot.py
@@ -2,8 +2,8 @@ from network import WLAN
 import pycom
 import machine
 import time
+
 wlan = WLAN(mode=WLAN.STA)
-RGB_RED = (RGB_BRIGHTNESS << 16)
 
 wlan.connect(ssid='Projet')
 while not wlan.isconnected():
@@ -11,12 +11,5 @@ while not wlan.isconnected():
 print("WiFi connected succesfully")
 print(wlan.ifconfig())
 
-pycom.rgbled(RGB_RED)
-time.sleep(1)
-pycom.rgbled(0x000000)
-time.sleep(1)
-pycom.rgbled(RGB_RED)
-time.sleep(1)
-pycom.rgbled(0x000000)
 
 machine.main('main.py') #lancement après le boot 
